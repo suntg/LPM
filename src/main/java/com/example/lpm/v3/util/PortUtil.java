d2c1d108af8f08de5308d6212698f0949f8a8081
sun <@>
2023-03-06 16:04:12 +0800

    ''

--- src/main/java/com/example/lpm/v3/util/PortUtil.java
+++ src/main/java/com/example/lpm/v3/util/PortUtil.java
@@ -1,19 +1,11 @@
 package com.example.lpm.v3.util;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PortUtil {
 
-    private final static List<Integer> COMMON_PORT = new ArrayList<>() {{
-        add(21888);
-        add(3306);
-        add(6379);
-        add(80);
-        add(9999);
-        add(28080);
-        add(18080);
-    }};
+    private final static List<Integer> COMMON_PORT = Arrays.asList(21888, 3306, 6379, 80, 9999, 28080, 18080, 443);
 
 
     public static Boolean contains(Integer port) {
